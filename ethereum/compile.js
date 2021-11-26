55555ecba953bf1e86eeafe89e122c4b6bd36391
Antonius Listyono <antonius.listyono_dxg@IU-052168.HOSDG>
2021-11-26 22:02:04 +0700

    initial commit

---
+++ ethereum/compile.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra'); //Give access to file system in local computer
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath); //ensure no directory and then create if not available
+
+console.log(output);
+for(let contract in output){
+  let fileName = contract.replace(':', ''); //Windows cannot use ':' as a part of filename
+  fs.outputJsonSync(
+    path.resolve(buildPath, fileName + '.json'),
+    output[contract]
+  );
+}
